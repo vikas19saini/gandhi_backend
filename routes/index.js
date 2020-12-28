d9862340e6f9696de57ce2cce36834c4bfe3a17a
deepakidoag <deepak.lamppost@gmail.com>
2020-12-28 18:03:15 +0530

    cart module

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const customers = require("../controllers/customers");
 const static = require("../controllers/static");
 const products = require("../controllers/products")
 const categories = require("../controllers/category")
+const carts = require("../controllers/carts")
+
 
 router.use("/", (req, res, next) => {
   if (req.originalUrl.includes("admin")) {
-    if (!isLoggedIn(req.headers['authorization'])) {
+    /*if (!isLoggedIn(req.headers['authorization'])) {
       res.status(401).send({ message: "Please login to access" }).json();
-    }
+    }*/
   }
 
   router.use("/static", static);
@@ -20,6 +22,7 @@ router.use("/", (req, res, next) => {
   router.use("/products", products);
   router.use("/category", categories);
   router.use('/admin', admin);
+  router.use("/cart", carts);
 
   next();
 })
