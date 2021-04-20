434a93bc7978a93e714e438ed575d8f8ea254cc1
unknown <saini.vikas630@gmail.com>
2021-04-20 14:08:10 +0530

    ..

--- controllers/payments.js
+++ controllers/payments.js
@@ -6,12 +6,12 @@ route.get("/config", async (req, res) => {
         mode: process.env.PAYPAL_MODE,
         key: process.env.PAYPAL_KEY,
         title: "Paypal Express Checkout"
-    }, {
+    }/* , {
         name: "bank",
         url: process.env.BANK_PAYMENT_URL,
         merchantId: process.env.BANK_PAYMENT_ID,
         title: "Pay by credit/debit card"
-    }];
+    } */];
 
     return res.json(payments);
 });
