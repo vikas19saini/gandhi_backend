2c5734081ac7e7f4b6890274d9b1e3df93e7bb2a
unknown <saini.vikas630@gmail.com>
2021-06-05 11:05:56 +0530

    download all products

--- controllers/admin/products.js
+++ controllers/admin/products.js
@@ -2,6 +2,7 @@ const route = require("express").Router();
 const { Products, ProductsAttributeValues, ProductsCategories, ProductsUploads, ProductsFilterValues } = require("../../models/index");
 const seqConnection = require("../../models/connection");
 const { Op } = require("sequelize");
+const { writeFileSync } = require("fs");
 
 route.get("/", async (req, res) => {
     let params = {
@@ -9,7 +10,7 @@ route.get("/", async (req, res) => {
         distinct: true,
         attributes: {
             exclude: ["longDescription", "metaDescription", "metaTitle", "minOrderQuantity", "maxOrderQuantity", "shippingHeight", "shippingLength", "shippingWeight",
-            "shortDescription", "step", "tags", "updatedAt", "lengthClassId", "taxClassId", "weightClassId" ]
+                "shortDescription", "step", "tags", "updatedAt", "lengthClassId", "taxClassId", "weightClassId"]
         }
     };
 
@@ -180,5 +181,71 @@ route.delete("/:id", async (req, res) => {
         return res.status(404).json(err);
     }
 });
+
+route.get("/download/all", async (req, res) => {
+    Products.findAll(
+        {
+            include: ["featuredImage", "thumbnails", "filters", "attributes", "categories"]
+        }
+    ).then((products) => {
+        let rows = [];
+        for (let product of products) {
+            let row = {
+                name: product.name,
+                sku: product.sku,
+                shortDescription: product.shortDescription,
+                longDescription: product.longDescription,
+                tags: product.tags,
+                ragularPrice: product.ragularPrice,
+                manageStock: product.manageStock,
+                quantity: product.quantity,
+                shippingWeight: product.shippingWeight,
+                status: product.status,
+                stockStatus: product.stockStatus,
+                image: product.featuredImage ? product.featuredImage.fullUrl : "NULL",
+                thumbnails: product.thumbnails.map(th => th.fullUrl).join("||"),
+                categories: product.categories.map(cat => cat.slug).join("||"),
+                filters: product.filters.map(fil => fil.name).join("||"),
+                attributes: product.attributes.map(att => att.name).join("||"),
+                attributeValues: product.attributes.map(att => att.productsAttributeValues.attributeDescription).join("||"),
+            }
+
+            rows.push(row);
+        }
+
+        let xlsx = require('json-as-xlsx');
+
+        var buffer = xlsx([
+            { label: 'sku', value: 'sku' },
+            { label: 'name', value: 'name' },
+            { label: 'shortDescription', value: 'shortDescription' },
+            { label: 'longDescription', value: 'longDescription' },
+            { label: 'tags', value: 'tags' },
+            { label: 'categories', value: 'categories' },
+            { label: 'attributes', value: 'attributes' },
+            { label: 'attributeValues', value: 'attributeValues' },
+            { label: 'filters', value: 'filters' },
+            { label: 'ragularPrice', value: 'ragularPrice' },
+            { label: 'manageStock', value: 'manageStock' },
+            { label: 'quantity', value: 'quantity' },
+            { label: 'shippingWeight', value: 'shippingWeight' },
+            { label: 'thumbnails', value: 'thumbnails' },
+            { label: 'image', value: 'image' },
+            { label: 'status', value: 'status' },
+            { label: 'stockStatus', value: 'stockStatus' },
+        ], rows, {
+            sheetName: 'Sheet1',
+            fileName: 'AllProducts',
+            extraLength: 10
+        }, false)
+
+
+        writeFileSync(`${process.env.IMPORT_DIR}/AllProducts.xlsx`, buffer);
+        return res.json({ status: "success" });
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({ status: "fail" });
+    })
+})
 
 module.exports = route;
